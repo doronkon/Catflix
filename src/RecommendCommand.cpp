3d3ec3de3a0e2bce109c4b94deff03c3628b4ce5
cartmanbra1 <royiamsalem@gmail.com>
2024-11-25 20:02:45 +0200

    Happy Thoughts and eating oranges

---
+++ src/RecommendCommand.cpp
@@ -0,0 +1,15 @@
+/*#include "ICommand.h"
+#define PATH "../data/userData.txt"
+
+class RecommendCommand : public ICommand {
+
+    void execute(vector<string>&& inputVector = vector<string>{}, 
+                            vector<User>&& users = vector<User>{}) {
+                                return;
+                            }
+    
+    void helpPrint() {
+        cout << "recommend [userid] [movieid]" << endl;
+    
+    }
+};*/
