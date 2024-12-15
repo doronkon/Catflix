d62809f7842494b212fb984a2e7cd2b9bb86d86c
Cartmanbra1 <royiamsalem@gmail.com>
2024-12-15 16:36:28 +0200

    docker working for server

--- Dockerfile
+++ Dockerfile
@@ -7,21 +7,21 @@ WORKDIR /Catflix
 # Step 3: Copy the entire project into the container
 COPY . /Catflix
 
-# Step 4: Install necessary dependencies (including CMake, build-essential, and Git)
+# Step 4: Install necessary dependencies (CMake, build-essential, Git, and Python)
 RUN apt-get update && apt-get install -y \
     build-essential \
     cmake \
     git \
     && rm -rf /var/lib/apt/lists/*
 
-# Step 5: Build the project using CMake (This includes both your app and tests)
+# Step 5: Build the project using CMake (ensure to generate and build the project)
 RUN cmake -S . -B /Catflix/build && cmake --build /Catflix/build
 
-# Step 6: Ensure the app binary and test binaries are executable
-RUN chmod +x /Catflix/build/runTests /Catflix/build/Catflix_app
+# Step 6: Ensure the Catflix_app binary is executable
+RUN chmod +x /Catflix/build/Catflix_app
 
-# Step 7: Set the entrypoint to allow running tests or the app
-ENTRYPOINT ["sh", "-c"]
+# Step 7: Set the entrypoint to the compiled server binary
+ENTRYPOINT ["/Catflix/build/Catflix_app"]
 
-# Step 8: Default to running tests first, if available; otherwise, run the app
-CMD ["/Catflix/build/runTests || /Catflix/build/Catflix_app"]
+# Step 8: Set the command for the server to listen on a port (default args can be overwritten)
+CMD ["127.0.0.1", "7071"]
--- src/tests/test.cpp
+++ src/tests/test.cpp
@@ -1,116 +1,116 @@
-// #include "test.h"
-// using namespace std;
+#include "test.h"
+using namespace std;
 
-// // Positive test case
-// TEST(utils, toNumberTest1)
-// {
-//     ID_TYPE number;
-//     EXPECT_EQ(util::toNumber("89", number), true);
-// }
+// Positive test case
+TEST(utils, toNumberTest1)
+{
+    ID_TYPE number;
+    EXPECT_EQ(util::toNumber("89", number), true);
+}
 
-// // Negative test case
-// TEST(utils, toNumberTest2)
-// {
-//     ID_TYPE number;
-//     EXPECT_EQ(util::toNumber("sffsdfd", number), false);
-// }
+// Negative test case
+TEST(utils, toNumberTest2)
+{
+    ID_TYPE number;
+    EXPECT_EQ(util::toNumber("sffsdfd", number), false);
+}
 
-// // Positive test case
-// TEST(utils, toNumberTest3)
-// {
-//     ID_TYPE number;
-//     util::toNumber("89", number);
-//     EXPECT_EQ(number,89);
-// }
+// Positive test case
+TEST(utils, toNumberTest3)
+{
+    ID_TYPE number;
+    util::toNumber("89", number);
+    EXPECT_EQ(number,89);
+}
 
-// // Positive test case
-// TEST(utills, findUserTest1)
-// {
-//     vector<User> users;
-//     ID_TYPE id = 200;
-//     // Creating a User object with both parameters
-//     users.push_back(User(id, {})); // Use empty movie list for now
-//     EXPECT_EQ(util::findUserByID(users, 200), 0);
-// }
+// Positive test case
+TEST(utills, findUserTest1)
+{
+    vector<User> users;
+    ID_TYPE id = 200;
+    // Creating a User object with both parameters
+    users.push_back(User(id, {})); // Use empty movie list for now
+    EXPECT_EQ(util::findUserByID(users, 200), 0);
+}
 
-// // Negative test case
-// TEST(utills, findUserTest2)
-// {
-//     vector<User> users;
-//     ID_TYPE id = 200;
-//     users.push_back(User(id, {}));
-//     EXPECT_EQ(util::findUserByID(users, 201), -1); // Corrected: should expect false
-// }
+// Negative test case
+TEST(utills, findUserTest2)
+{
+    vector<User> users;
+    ID_TYPE id = 200;
+    users.push_back(User(id, {}));
+    EXPECT_EQ(util::findUserByID(users, 201), -1); // Corrected: should expect false
+}
 
-// // Positive test case
-// TEST(utills, ChangeVectorTest1)
-// {
-//     vector<string> strings;
-//     strings.push_back("80"); 
-//     vector<ID_TYPE> vec = util::changeVectorType(strings);
-//     EXPECT_EQ(vec[0], 80);
-// }
+// Positive test case
+TEST(utills, ChangeVectorTest1)
+{
+    vector<string> strings;
+    strings.push_back("80"); 
+    vector<ID_TYPE> vec = util::changeVectorType(strings);
+    EXPECT_EQ(vec[0], 80);
+}
 
-// // Negative test case
-// TEST(utills, ChangeVectorTest2)
-// {
-//     vector<string> strings;
-//     strings.push_back("aba"); 
-//     vector<ID_TYPE> vec = util::changeVectorType(strings);
-//     EXPECT_EQ(vec.empty(), true);
-// }
+// Negative test case
+TEST(utills, ChangeVectorTest2)
+{
+    vector<string> strings;
+    strings.push_back("aba"); 
+    vector<ID_TYPE> vec = util::changeVectorType(strings);
+    EXPECT_EQ(vec.empty(), true);
+}
 
-// // Positive test case
-// TEST(Movies, biggerTest1)
-// {
-//     Movie movie1(124);
-//     Movie movie2(123);
-//     EXPECT_EQ(movie1.bigger(movie2), true);
-// }
+// Positive test case
+TEST(Movies, biggerTest1)
+{
+    Movie movie1(124);
+    Movie movie2(123);
+    EXPECT_EQ(movie1.bigger(movie2), true);
+}
 
-// // Negative test case
-// TEST(Movies, biggerTest2)
-// {
-//     Movie movie1(12);
-//     Movie movie2(123);
-//     EXPECT_EQ(movie1.bigger(movie2), false);
-// }
+// Negative test case
+TEST(Movies, biggerTest2)
+{
+    Movie movie1(12);
+    Movie movie2(123);
+    EXPECT_EQ(movie1.bigger(movie2), false);
+}
 
-// // Positive test case
-// TEST(Users, getUserIDTest1)
-// {
-//     ID_TYPE id = 103;
-//     vector<Movie> movies; // Create empty movie list
-//     User user1(id, movies); // Pass movie list to constructor
-//     EXPECT_EQ(user1.getUserId(), id);
-// }
+// Positive test case
+TEST(Users, getUserIDTest1)
+{
+    ID_TYPE id = 103;
+    vector<Movie> movies; // Create empty movie list
+    User user1(id, movies); // Pass movie list to constructor
+    EXPECT_EQ(user1.getUserId(), id);
+}
 
-// // Negative test case
-// TEST(Users, getUserIDTest2)
-// {
-//     ID_TYPE id = 300;
-//     vector<Movie> movies; // Create empty movie list
-//     User user1(id, movies); // Pass movie list to constructor
-//     EXPECT_EQ(user1.getUserId() == id - 1, false);
-// }
+// Negative test case
+TEST(Users, getUserIDTest2)
+{
+    ID_TYPE id = 300;
+    vector<Movie> movies; // Create empty movie list
+    User user1(id, movies); // Pass movie list to constructor
+    EXPECT_EQ(user1.getUserId() == id - 1, false);
+}
 
-// // Positive test case
-// TEST(Users, didIWatchTest1)
-// {
-//     ID_TYPE id = 103;
-//     vector<Movie> movies; // Create empty movie list
-//     User user1(id, movies);
-//     Movie movie1(id + 1);
-//     user1.addMovie(movie1); 
-//     EXPECT_EQ(user1.didIWatch(movie1), true);
-// }
+// Positive test case
+TEST(Users, didIWatchTest1)
+{
+    ID_TYPE id = 103;
+    vector<Movie> movies; // Create empty movie list
+    User user1(id, movies);
+    Movie movie1(id + 1);
+    user1.addMovie(movie1); 
+    EXPECT_EQ(user1.didIWatch(movie1), true);
+}
 
-// // Negative test case
-// TEST(Users, didIWatchTest2)
-// {
-//     ID_TYPE id = 103;
-//     vector<Movie> movies; // Create empty movie list
-//     User user1(id, movies);
-//     Movie movie1(id + 1);
-//     EXPECT_EQ(user1.didIWatch(movie1), false);
-// }
+// Negative test case
+TEST(Users, didIWatchTest2)
+{
+    ID_TYPE id = 103;
+    vector<Movie> movies; // Create empty movie list
+    User user1(id, movies);
+    Movie movie1(id + 1);
+    EXPECT_EQ(user1.didIWatch(movie1), false);
+}
