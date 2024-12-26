c8d36f63dd6ab98c54f7ec994387369da5a249f7
Nadav Rutman Moshe <nadav8333@gmail.com>
2024-12-26 12:55:28 +0200

    deleting debug print

--- part3/controllers/token.js
+++ part3/controllers/token.js
@@ -1,7 +1,6 @@
 const tokenService = require('../services/token');
 
 const validateUser = async (req, res) => {
-    console.log('headers: ',req.headers['user'],req.headers['password'])
     const user = await tokenService.validateUser(req.headers['user'],req.headers['password']);
     if (user.error)
     {
