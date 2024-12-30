aef8082859ff9c64c2b16810ba4102e59204cb5f
doronkon <doronkon57@gmail.com>
2024-12-30 11:29:32 +0200

    cpp recommendation system now works with userId and movieId

--- part3/controllers/recommend.js
+++ part3/controllers/recommend.js
@@ -1,15 +1,15 @@
-const userService = require('../services/recommend');
+const recommendService = require('../services/recommend');
 
 const getRecommendation = async (req,res) => {
     const UserID = req.headers['user']
     const MovieID = req.params.id
-    const response = await userService.getRecommendation(UserID,MovieID)
+    const response = await recommendService.getRecommendation(UserID,MovieID)
     res.json(response)
 }
 const addMovie = async (req,res) => {
     const UserID = req.headers['user']
     const MovieID = req.params.id
-    const response = await userService.addMovie(UserID,MovieID)
+    const response = await recommendService.addMovie(UserID,MovieID)
     res.status(parseInt(response.slice(0, 3))).json();
 
 }
--- part3/services/recommend.js
+++ part3/services/recommend.js
@@ -1,4 +1,6 @@
 const net = require('net');
+const User = require('../models/user');
+const Movie = require('../models/movie')
 
 const sendToServer = (message) => {
     return new Promise((resolve, reject) => {
@@ -20,14 +22,63 @@ const sendToServer = (message) => {
     });
 }
 
+const splitString = async (response) => {
+    
+    // Match everything between the second \n and the last \n
+    const match = response.match(/(?<=\n\n)(.*)/);
+    if (!match) {
+        return []; // Return empty array if no match
+    }
+    
+    const moviesString = match[0].trim();  // Get the matched string and remove any leading/trailing spaces
+    
+    // Split the string by spaces to get movie IDs
+    const moviesArray = moviesString.split(/\s+/);
+    
+    // Convert each movie ID to an actual number
+    const actualMovieId = moviesArray.map(movie => parseInt(movie, 10));
 
-const getRecommendation = async (UserID,MovieID) => {
+    const moviesToReturn = [];
+    
+    // Iterate through the movie IDs and fetch the corresponding movies
+    for (let movieId of actualMovieId) {
+        const currMovie = await Movie.findOne({ movieId: movieId });
+        
+        // If the movie exists, add it to the result array
+        if (currMovie) {
+            moviesToReturn.push(currMovie);
+        }
+    }
+    return moviesToReturn;
+};
 
-}
-const addMovie = async (UserID,MovieID) => {
-    var response = await sendToServer('PATCH ' + UserID + ' ' + MovieID + '\n');
+const getRecommendation = async (currUser,currMovie) => {
+    const user = await User.findOne({_id:currUser});
+    const movie = await Movie.findOne({_id:currMovie});
+    // the user we want to recommend to or the movie we want to recommend on does not exist
+    if(!user || !movie){
+        return null;
+    }
+    const userId = user.userId;
+    const movieId = movie.movieId;
+    const response =  await sendToServer('GET ' + userId + ' ' + movieId + '\n');
     if(response[0] == '4'){
-         response = await sendToServer('POST ' + UserID + ' ' + MovieID + '\n');
+        return response;
+    }
+    return splitString(response);
+}
+const addMovie = async (currUser,currMovie) => {
+    const user = await User.findOne({_id:currUser});
+    const movie = await Movie.findOne({_id:currMovie});
+    // the user we want to recommend to or the movie we want to recommend on does not exist
+    if(!user || !movie){
+        return null;
+    }
+    const userId = user.userId;
+    const movieId = movie.movieId;
+    var response = await sendToServer('PATCH ' + userId + ' ' + movieId + '\n');
+    if(response[0] == '4'){
+         response = await sendToServer('POST ' + userId + ' ' + movieId + '\n');
     }
     return response;
 }
