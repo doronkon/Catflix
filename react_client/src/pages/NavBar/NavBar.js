95a2f69c27ea901bbc032b571b02bcb27436cce6
Nadav Rutman Moshe <nadav8333@gmail.com>
2025-01-17 15:19:29 +0200

    removing pictures

--- react_client/src/pages/NavBar/NavBar.js
+++ react_client/src/pages/NavBar/NavBar.js
@@ -9,7 +9,7 @@ function NavBar() {
                 <Link to="/">Home</Link>
                 <Link to="/profile">Profile</Link>
                 <Link to="/search">Search</Link>
-                {<Link to="/test">Upload just test all hard coded</Link>}
+                {<Link to="/UploadMovie">Upload just test all hard coded</Link>}
 
             </nav>
         </header>
